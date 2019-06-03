79f9806b30c6756c2d763c55ab01f428f784854c
Calum Darroch <calum_darroch@hotmail.com>
2019-06-03 12:32:47 +0100

    First test for Transaction class written and passed, time successfully stubbed

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,13 @@
+require 'transaction'
+require 'date'
+
+describe Transaction do
+
+  describe '#initialize' do
+    it 'initializes with a date in %d-%m-%Y format' do
+      allow(Time).to receive(:now).and_return Time.new(2007,04,10, 12,0,0, "+01:00")
+      expect(subject.date).to eq('10-04-2007')
+    end
+  end
+
+end
