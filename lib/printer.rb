3d5d64c0ee636c51a3033da40c1d9253770f487a
Calum Darroch <calum_darroch@hotmail.com>
2019-06-04 17:10:28 +0100

    Printer #print_statement test added, tested for #puts calls to STDOUT

--- lib/printer.rb
+++ lib/printer.rb
@@ -8,4 +8,17 @@ class Printer
     @account = Account.new
   end
 
+  def print_statement
+    puts 'date || credit || debit || balance'
+    @account.transactions.reverse.each do |transaction|
+      if transaction.amount.positive?
+        puts "#{transaction.date} || #{transaction.amount} || || #{account.balance}"
+      elsif transaction.amount.negative?
+        puts "#{transaction.date} || || #{transaction.amount.abs} || #{account.balance}"
+      else
+        puts "#{transaction.date} || || || #{account.balance}"
+      end
+    end
+  end  
+
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -8,4 +8,13 @@ describe Printer do
     end
   end
 
+  describe '#print_statement' do
+    it 'should print a header line and a line for each transaction' do
+      subject.account.deposit(500)
+      subject.account.withdraw(200)
+      expect(STDOUT).to receive(:puts).exactly(3).times
+      subject.print_statement
+    end
+  end
+
 end
